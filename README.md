6508528b1adcb90292dda9c97a62d51645e000d1
KirBotz <92134398+KirBotz@users.noreply.github.com>
2022-12-24 16:21:03 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# themaptero
+TemuxOS Themes
+Instalação do script:
+```sh
+bash <(curl https://raw.githubusercontent.com/KirBotz/themaptero/main/menu.sh)
+```
+
---
+++ menu.sh
@@ -0,0 +1,47 @@
+if (( $EUID != 0 )); then
+    echo -e "${CYAN}Run script as root"
+    exit
+fi
+
+clear
+
+portuguese(){
+    bash <(curl https://raw.githubusercontent.com/KirBotz/themaptero/main/instaladorportugues.sh)
+}
+
+german(){
+    bash <(curl https://raw.githubusercontent.com/KirBotz/themaptero/main/instaladorgerman.sh)
+}
+
+english(){
+    bash <(curl https://raw.githubusercontent.com/KirBotz/themaptero/main/instaladorenglish.sh)
+}
+
+    CYAN='\033[0;36m'
+    echo -e "${CYAN}Copyright (c) 2022 TemuxOS"
+    echo -e "${CYAN}This Software is opensource."
+    echo -e "Theme Instalator"
+    echo -e "${CYAN}Youtube: https://youtube.com/@kangbotwhatsapp"
+    echo -e "Select your Language"
+    echo -e "${CYAN}[1] Portuguese"
+    echo -e "${CYAN}[2] German"
+    echo -e "${CYAN}[3] English"
+    echo -e "${CYAN}[4] Exit"
+    
+read -p "Insira um numero: " choice
+if [ $choice == "1" ]
+    then
+    portuguese
+fi
+if [ $choice == "2" ]
+    then
+    german
+fi
+if [ $choice == "3" ]
+    then
+    english
+fi
+if [ $choice == "4" ]
+    then
+    exit
+fi
